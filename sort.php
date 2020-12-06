264c7f2862f1650152a06a18823b7e0f9253acf2
Chuck Youngfeldt <Chuck.Youngfeldt@nomissolutions.com>
2020-12-06 17:51:48 -0500

    adding unsanitized input

--- sort.php
+++ sort.php
@@ -1,4 +1,6 @@
 <?php
+$var = $_POST['var'];
+mysql_query("SELECT * FROM sometable WHERE id = $var");
 $freecodecamp = array("zero"=>"free", "one"=>"code", "two"=>"camp");
 ksort($freecodecamp);
 print_r($freecodecamp);
